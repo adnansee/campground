573151eb9332d32142430d638c39c590defbe0ed
adnansee <adnansee@gmail.com>
2020-04-29 16:29:49 +0200

    DBSeeder added1.1

--- src/main/java/com/base/camping/campground/DBSeeder.java
+++ src/main/java/com/base/camping/campground/DBSeeder.java
@@ -23,23 +23,39 @@ public class DBSeeder implements CommandLineRunner {
     @Override
     public void run(String... args) {
 
-        Campgrounds c = new Campgrounds();
-        c.setDescription("NICE");
-        c.setEmail("X@X");
-        c.setFire("YES");
-        c.setLocation("X-cord, Y-cord");
-        c.setMaxdays(3);
-        c.setName("HAPPY VALLY");
-        c.setPeople(3);
-        c.setPic("https://picsum.photos/200/300");
-        c.setReview("GREAT");
-        c.setSize(50);
-        c.setToilet("YES");
-        c.setVehicle("YES");
-        c.setPrice(20);
+        Campgrounds campground1 = new Campgrounds();
+        campground1.setDescription("NICE");
+        campground1.setEmail("X@X");
+        campground1.setFire("YES");
+        campground1.setLocation("X-cord, Y-cord");
+        campground1.setMaxdays(3);
+        campground1.setName("HAPPY VALLY");
+        campground1.setPeople(3);
+        campground1.setPic("https://picsum.photos/200/300");
+        campground1.setReview("GREAT");
+        campground1.setSize(50);
+        campground1.setToilet("YES");
+        campground1.setVehicle("YES");
+        campground1.setPrice(20);
 
 
-        List<Campgrounds> camps = Arrays.asList(c);
+        Campgrounds campground2 = new Campgrounds();
+        campground2.setDescription("LOVELY");
+        campground2.setEmail("Y@Y");
+        campground2.setFire("NO");
+        campground2.setLocation("X-cord, Y-cord");
+        campground2.setMaxdays(5);
+        campground2.setName("RIDGE VIEW");
+        campground2.setPeople(2);
+        campground2.setPic("https://picsum.photos/200/300");
+        campground2.setReview("AWESOME");
+        campground2.setSize(30);
+        campground2.setToilet("NO");
+        campground2.setVehicle("YES");
+        campground2.setPrice(35);
+
+
+        List<Campgrounds> camps = Arrays.asList(campground1, campground2);
         this.campRepository.saveAll(camps);
     }
     }
